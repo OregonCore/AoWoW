df9cd902f706fe09a3e1f9deaa13e32672d5a2dd
obana <ekzobam@gmail.com>
2016-01-31 19:09:12 +0500

    [Comments]: Now correct display comment time

--- tools/sql/comments.sql
+++ tools/sql/comments.sql
@@ -3,11 +3,11 @@ CREATE TABLE IF NOT EXISTS  `aowow_comments` (
   `type` int(10) unsigned NOT NULL COMMENT 'Type of Page',
   `typeid` int(10) unsigned NOT NULL COMMENT 'ID Of Page',
   `userid` bigint(20) unsigned NOT NULL COMMENT 'User ID',
-  `post_date` timestamp NOT NULL default CURRENT_TIMESTAMP COMMENT 'Comment timestap',
+  `post_date` int(10) unsigned NOT NULL default 0 COMMENT 'Comment timestap',
   `commentbody` text COMMENT 'Comment text',
   `replyto` bigint(20) unsigned default NULL COMMENT 'Reply To, comment ID',
   `edit_userid` bigint(20) unsigned NOT NULL default 0 COMMENT 'Last Edit User ID',
-  `edit_date` timestamp NOT NULL default 0 COMMENT 'Last Edit Time',
+  `edit_date` int(10) unsigned NOT NULL default 0 COMMENT 'Last Edit Time',
   `edit_count` int(10) unsigned NOT NULL default 0 COMMENT 'Count Of Edits',
   UNIQUE KEY `id` (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=248 DEFAULT CHARSET=utf8 COMMENT='AoWoW Comments Table';
